6f524f1590a2da83850064aa902a592fc9f9c4c9
jbk2 <james@humanbenefits.com>
2024-01-23 21:56:47 +0100

    Styling all views
    - Introduce external google fonts
    - Extend Tailwind.config.js colour and fonts
    - Extend Tailwind base
    - Format tables, headers, labels, inputs

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,14 +5,21 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         display: ['Racing Sans One', 'sans-serif'],
-        label: ['Nunito Sans', 'sans-serif']
+        label: ['Nunito Sans', 'sans-serif'],
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'header-green': '#14453D',
+        'light-blue': '#42BFDD',
+        'med-green': '#C84630',
       },
     },
   },
