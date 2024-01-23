c576944d0fe0cf99df111169672cdbccd2f70444
jbk2 <james@humanbenefits.com>
2024-01-23 09:46:28 +0100

    Create-Bookings controller|Bookings model|Passenger model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -14,6 +14,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+  has_many :bookings
 
   delegate :country, :code, :city, :name, to: :departure_airport, prefix: true
   delegate :country, :code, :city, :name, to: :arrival_airport, prefix: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_14_153128) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_23_081821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_14_153128) do
     t.datetime "updated_at", null: false
     t.string "country"
     t.string "city"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "no_of_passengers"
+    t.bigint "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -36,6 +44,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_14_153128) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_14_153128) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key "passengers", "bookings"
 end
