48dc1d4add3aec074458b3901e98169a9d5f7505
jbk2 <james@humanbenefits.com>
2024-02-06 22:26:15 +0100

    Refactor password visibility checkbox & layout styling
    - Change the password form to use the new modal controller
    - Rename togglePassword to showPassword
    - Styling of password visibility checkbox and error messages
    - Chaneg bg image styling that was restricting vertical scrolling

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,9 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="modal"
 export default class extends Controller {
+
+  static targets = ["password"]
+  
   connect() {
     console.log("Modal controller connected");
+    console.log(`Password target found; ${this.hasPasswordTarget}`);
     this.element.querySelector('dialog').showModal();
   }
+
+  showPassword() {
+    if (this.passwordTarget.type === "password") {
+      this.passwordTarget.type = "text";
+    } else {
+      this.passwordTarget.type = "password";
+    }
+  }
+
 }
