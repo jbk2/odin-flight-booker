1eeb90721bec3c1a8e0049db6a89a61dced8cb7e
jbk2 <james@humanbenefits.com>
2024-02-07 09:39:51 +0100

    Add password visibility by icon
    - Add ModdalController#togglePasswordVisibility to toggle visibility class
      of two eye icon SVGs in the password html modal.

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,19 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
-  static targets = ["password"]
+  static targets = ["passengerPassword", "eye", "eyeSlash", "iconDiv"]
   
   connect() {
     console.log("Modal controller connected");
-    console.log(`Password target found; ${this.hasPasswordTarget}`);
+    console.log(`passengerPassword target found; ${this.hasPassengerPasswordTarget}`);
+    console.log(`Eye target found; ${this.hasEyeTarget}`);
+    console.log(`Eye-slash target found; ${this.hasEyeSlashTarget}`);
+    console.log(`Icon-Div target found; ${this.hasIconDivTarget}`);
     this.element.querySelector('dialog').showModal();
   }
 
-  showPassword() {
-    if (this.passwordTarget.type === "password") {
-      this.passwordTarget.type = "text";
+  toggleIcon() {
+    const passwordField = this.passengerPasswordTarget;
+    const eyeIcon = this.eyeTarget;
+    const eyeSlashIcon = this.eyeSlashTarget;
+    const iconDiv = this.iconDivTarget;
+
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      eyeSlashIcon.classList.remove("invisible");
+      eyeSlashIcon.classList.add("visible");
+      eyeIcon.classList.remove("visible");
+      eyeIcon.classList.add("invisible");
     } else {
-      this.passwordTarget.type = "password";
+      passwordField.type = "password";
+      eyeIcon.classList.remove("invisible");
+      eyeIcon.classList.add("visible");
+      eyeSlashIcon.classList.remove("visible");
+      eyeSlashIcon.classList.add("invisible");
     }
   }
 
