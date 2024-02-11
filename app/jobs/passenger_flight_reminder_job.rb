c27df5ac88333a807a1b7524b2c76288a28837ac
jbk2 <james@humanbenefits.com>
2024-02-11 11:00:28 +0100

    Log Job failures to Sidekiq

--- app/jobs/passenger_flight_reminder_job.rb
+++ app/jobs/passenger_flight_reminder_job.rb
@@ -2,6 +2,10 @@ class PassengerFlightReminderJob < ApplicationJob
   queue_as :default
   sidekiq_options retry: 3
 
+  sidekiq_retries_exhausted do |job, ex|
+    Sidekiq.logger.warn "Failed #{job['class']} with #{job['args']}: #{job['error_message']}"
+  end
+
   def perform(booking_id)
     booking = Booking.find(booking_id)
     return Rails.logger.info "Booking ##{booking_id} not found" unless booking
