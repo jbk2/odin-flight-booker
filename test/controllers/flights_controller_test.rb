6e323cd02f4b459038b85489845d28d3b0eb904d
jbk2 <james@humanbenefits.com>
2024-02-11 11:45:20 +0100

    Remove test/, create spec for flight_reminder_job

--- test/controllers/flights_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class FlightsControllerTest < ActionDispatch::IntegrationTest
-  test "should get search" do
-    get flights_search_url
-    assert_response :success
-  end
-end
