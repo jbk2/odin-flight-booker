9e3055958da9c3d5dfaec95132f34d518996ba61
jbk2 <james@humanbenefits.com>
2024-02-11 12:43:58 +0100

    Test Airport validations and associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -14,6 +14,7 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: 'Flight', foreign_key: 'departure_airport_id'
   has_many :arriving_flights, class_name: 'Flight', foreign_key: 'arrival_airport_id'
+  validates :name, :code, :country_id, :country, :city, presence: true
 
   def formatted_airport_name_label
     if name.include?(city)
--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Airport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  RSpec.shared_examples 'an airport with mandatory attributes' do |attribute|
+    it "is invalid without a #{attribute}" do
+      airport = Airport.new(valid_attributes.except(attribute))
+      expect(airport).to be_invalid
+      expect(airport.errors[attribute]).to include("can't be blank")
+    end
+  end
+
+  describe 'validations' do
+    let(:valid_attributes) {
+      { name: 'Parisian Airport', code:'0001', country_id: '01', country: 'France', city: 'Paris' }
+    }
+
+    [:name, :code, :country_id, :country, :city].each do |attribute|
+      include_examples 'an airport with mandatory attributes', attribute
+    end
+  end
+
+  describe 'associations' do
+    # it { should have_many(:departing_flights).class_name('Flight').with_foreign_key('departure_airport_id') } # shoulda-matchers implementation
+    # it { should have_many(:arriving_flights).class_name('Flight').with_foreign_key('arrival_airport_id') } # shoulda-matchers implementation
+    let(:airport_1) { Airport.create(name: 'Parisian Airport', code:'0001', country_id: '01', country: 'France', city: 'Paris') }
+    let(:airport_2) { Airport.create(name: 'Spanish Airport', code:'0002', country_id: '02', country: 'Spain', city: 'Madrid') }
+    let(:flight) { Flight.create(departure_airport: airport_1, arrival_airport: airport_2, departure_time: Time.now + 1.hour , arrival_time: Time.now + 3.hours) }
+
+    it 'has many arriving flights' do
+      expect(airport_2.arriving_flights).to include(flight)
+    end
+    
+    it 'has many departing flights' do
+      expect(airport_1.departing_flights).to include(flight)
+    end
+  end
+
+  describe 'formatted_airport_name_label' do
+    context 'when the airport name includes the city' do
+      let(:airport_with_name_inc_city) { Airport.create(name: 'Madrid airport', code:'0002', country_id: '02', country: 'Spain', city: 'Madrid') }
+      it "returns the name unchanged" do
+        expect(airport_with_name_inc_city.formatted_airport_name_label).to eq('Madrid airport')
+      end
+    end
+
+    context 'when the airport name does not include the city' do
+      let(:airport_without_city_in_name) { Airport.create(name: 'Madrillian airport', code:'0002', country_id: '02', country: 'Spain', city: 'Madrid') }
+      it "returns the name prefixed with with the city and a tilde" do
+        expect(airport_without_city_in_name.formatted_airport_name_label).to eq('Madrid ~-~ Madrillian airport')
+      end
+    end
+  end
+    
 end
