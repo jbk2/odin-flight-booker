6974b32765ee7b1ee9230a2e3e2df0d2f1e1af81
jbk2 <james@humanbenefits.com>
2024-02-11 20:27:51 +0100

    Complete passing Booking validation tests

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -13,6 +13,7 @@
 #  remember_created_at    :datetime
 #
 require 'rails_helper'
+require 'securerandom'
 
 RSpec.describe Passenger, type: :model do
   include_context 'common setup'
@@ -29,6 +30,27 @@ RSpec.describe Passenger, type: :model do
         expect(passenger_2).to be_valid
       end
     end
+    context 'with a name <2 chars' do
+      let(:one_char_passenger) { Passenger.new(email: 'p1@test.com', name: 'a') }
+      it 'is invalid' do
+        expect(one_char_passenger).to be_invalid
+      end
+    end
+    context 'with a name >50 chars' do
+      too_long_name = SecureRandom.alphanumeric(55)
+      let(:loads_chars_passenger) { Passenger.new(email: 'p1@test.com', name: too_long_name) }
+      it 'is invalid' do
+        expect(loads_chars_passenger).to be_invalid
+      end
+    end
+    context 'with a name 2-50 chars' do
+      correct_length_name = SecureRandom.alphanumeric(25)
+      let(:loads_chars_passenger) { Passenger.new(email: 'p1@test.com', name: correct_length_name) }
+      it 'is valid' do
+        expect(loads_chars_passenger).to be_valid
+      end
+    end
+
     context 'without an email' do
       let(:no_email_passenger) { Passenger.new(name: 'p1') }
       it 'is invalid' do
@@ -40,6 +62,10 @@ RSpec.describe Passenger, type: :model do
         expect(passenger_2).to be_valid
       end
     end
+  end
+
+  describe 'associations' do
+    
   end
 
 end
