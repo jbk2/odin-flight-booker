5662715c78169a909e0698c356b856085796c288
jbk2 <james@humanbenefits.com>
2024-02-15 14:42:48 +0100

    Generate passengers and flight system test scaffolds

--- spec/system/flights_spec.rb
+++ spec/system/flights_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe "Flights", type: :system do
     driven_by(:rack_test)
   end
 
-  # pending "add some scenarios (or delete) #{__FILE__}"
+  # - navigate to the flights index / root page
+  #   - select departure country and check that airports.where country
+  #   - select arrival country and check that airports.where country
+  #   - ensure that the dates with flights are selectable
+  #   - click search button and ensure that flight results appear
+  #   - select a flight and ensure that the booking page appears
+  #   - ensure that you can add and remove passengers
+  #   - select book flight and ensure that password modal appears
+  #   - complete password and ensure that it goes to booking show
 end
---
+++ spec/system/passengers_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Passengers", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  # - check that a passenger can sign up outside of the booking flow
+end
